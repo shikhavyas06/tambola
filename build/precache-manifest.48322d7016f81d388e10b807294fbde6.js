fe2a8f45558909e92a620063d38dc127150d6980
Shikha Vyas <Shikha.Vyas@team.neustar>
2020-04-02 20:05:09 +0530

    production build

---
+++ build/precache-manifest.48322d7016f81d388e10b807294fbde6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "256b1ee584c7ea0fffd1682615128d35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6c45d2c427f35075317",
+    "url": "/static/css/main.a708a855.chunk.css"
+  },
+  {
+    "revision": "cc86f92a0e9b1ed7b6f7",
+    "url": "/static/js/2.1b35cb52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1b35cb52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6c45d2c427f35075317",
+    "url": "/static/js/main.16802061.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
